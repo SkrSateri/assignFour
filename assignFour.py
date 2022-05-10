880d670374124837c68d572a59ea0b9b92c9c153
SkrSateri <sarpython013@gmail.com>
2022-05-11 00:31:39 +0300

    Assignment has been complated.

--- assignFour.py
+++ assignFour.py
@@ -68,12 +68,20 @@ def init_matrix(k):# to initialise one dinensional matrix to use in decyription 
 
 def convert_key(plainI):# This function is going to convert a key into a workable input,(removing spaces, changing letter j with i ...etc).
     workableT = ""
-    for x in plainI:
+    for x in plainI.lower():
         if x.upper() == "J":
             workableT = workableT + "I"
         elif x != x.upper():
             workableT = workableT + x.upper()
-    print(workableT)
+    #print(workableT)
+    return workableT
+
+def convert_cipher_input(cipherI): # This function will converty cipher inputer into a workable string. Like its going to remove the numbers and space if exist
+    workableT = ""
+    indexTracker = 0
+    for x in cipherI.lower():
+        if x.upper() != x:
+            workableT = workableT + x.upper()
     return workableT
 
 def convert_plain_input(plainI):# This function is going to convert a plain input into a workable input,(removing spaces, changing letter j with i ...etc).
@@ -84,7 +92,7 @@ def convert_plain_input(plainI):# This function is going to convert a plain inpu
     plainILength = 0
     #first convertion rule for changing letter j and 
     #removing numbers + symbols from it
-    for x in plainI:
+    for x in plainI.lower():
         if x.upper() == "J":
             workableT = workableT + "I"
         elif x != x.upper():
@@ -93,7 +101,7 @@ def convert_plain_input(plainI):# This function is going to convert a plain inpu
     while (indexTracker < plainILength):
         try:
             twoLetters = workableT[indexTracker] + workableT[indexTracker + 1 ]
-            print(twoLetters)
+            #print(twoLetters)
             if twoLetters[0] == twoLetters[1]:
                 twoLetters = workableT[indexTracker] + "X"
                 tempText = tempText + twoLetters
@@ -103,7 +111,7 @@ def convert_plain_input(plainI):# This function is going to convert a plain inpu
                 indexTracker = indexTracker + 2
             twoLetters = ""
         except:
-            print("out of list range ")
+            #print("out of list range ")
             twoLetters = workableT[plainILength-1] + "X"
             tempText = tempText + twoLetters
             break
@@ -111,7 +119,7 @@ def convert_plain_input(plainI):# This function is going to convert a plain inpu
     #print(workableT)
     return workableT
     
-def encrypt_plain_input(plainI):
+def encrypt_plain_input(plainI): # the function to encrypt plain text
     indexTracker = 0
     encryptedText = ""
     twoLetters = ""
@@ -126,11 +134,7 @@ def encrypt_plain_input(plainI):
         letterTwo = twoLetters[1]
         letterTwoColumn = get_letter_multi_index_column(letterTwo)
         letterTwoRow = get_letter_multi_index_row(letterTwo)
-        print(twoLetters)
-        print(get_letter_multi_index_column(twoLetters[0]))#error here you cant get the column right
-        print(get_letter_multi_index_column(twoLetters[1]))
         if letterOneColumn == letterTwoColumn: #if both letters are in same column
-            print("in same column")
             if letterOneRow + 1 > 4:
                 encryptedText = encryptedText + fiveDMatrix[0][letterOneColumn] + fiveDMatrix[letterTwoRow + 1][letterTwoColumn]
             elif letterTwoRow + 1 > 4:
@@ -138,30 +142,64 @@ def encrypt_plain_input(plainI):
             else:
                 encryptedText = encryptedText + fiveDMatrix[letterOneRow + 1][letterOneColumn] + fiveDMatrix[letterTwoRow + 1][letterTwoColumn]
         elif letterOneRow == letterTwoRow: # if both letters are in same row
-            print("in same row")
             if letterOneColumn + 1 > 4:
                 encryptedText = encryptedText + fiveDMatrix[letterOneRow][0] + fiveDMatrix[letterTwoRow][letterTwoColumn + 1]
             elif letterTwoColumn + 1 > 4:
                 encryptedText = encryptedText + fiveDMatrix[letterOneRow][letterOneColumn + 1] + fiveDMatrix[letterTwoRow][0]
             else:
-                print("both letters are in same row rule else section")
                 encryptedText = encryptedText + fiveDMatrix[letterOneRow][letterOneColumn + 1] + fiveDMatrix[letterTwoRow][letterTwoColumn + 1]
-        else:
-            print("cross rule")
+        else: #if the letters are positioned in cross corners
             encryptedText = encryptedText + fiveDMatrix[letterOneRow][letterTwoColumn] + fiveDMatrix[letterTwoRow][letterOneColumn]
         indexTracker = indexTracker + 2
-    print(encryptedText)
+    return encryptedText
+
+def decrypt_plain_input(plainI):# the function to decrypt plain text
+    indexTracker = 0
+    decryptedText = ""
+    twoLetters = ""
+    letterOne = ""
+    letterTwo = ""
+    plainILength = len(plainI)
+    while (indexTracker < plainILength):
+        twoLetters = plainI[indexTracker] + plainI[indexTracker+1]
+        letterOne = twoLetters[0]
+        letterOneColumn = get_letter_multi_index_column(letterOne)
+        letterOneRow = get_letter_multi_index_row(letterOne)
+        letterTwo = twoLetters[1]
+        letterTwoColumn = get_letter_multi_index_column(letterTwo)
+        letterTwoRow = get_letter_multi_index_row(letterTwo)
+        if letterOneColumn == letterTwoColumn: #if both letters are in same column
+            if letterOneRow - 1 < 0:
+                decryptedText = decryptedText + fiveDMatrix[4][letterOneColumn] + fiveDMatrix[letterTwoRow - 1][letterTwoColumn]
+            elif letterTwoRow - 1 < 0:
+                decryptedText = decryptedText + fiveDMatrix[letterOneRow - 1][letterOneColumn] + fiveDMatrix[4][letterTwoColumn]
+            else:
+                decryptedText = decryptedText + fiveDMatrix[letterOneRow - 1][letterOneColumn] + fiveDMatrix[letterTwoRow - 1][letterTwoColumn]
+        elif letterOneRow == letterTwoRow: # if both letters are in same row
+            if letterOneColumn - 1 < 0:
+                decryptedText = decryptedText + fiveDMatrix[letterOneRow][4] + fiveDMatrix[letterTwoRow][letterTwoColumn - 1]
+            elif letterTwoColumn - 1 < 0:
+                decryptedText = decryptedText + fiveDMatrix[letterOneRow][letterOneColumn - 1] + fiveDMatrix[letterTwoRow][4]
+            else:
+                decryptedText = decryptedText + fiveDMatrix[letterOneRow][letterOneColumn - 1] + fiveDMatrix[letterTwoRow][letterTwoColumn - 1]
+        else: #if the letters are positioned in cross corners
+            decryptedText = decryptedText + fiveDMatrix[letterOneRow][letterTwoColumn] + fiveDMatrix[letterTwoRow][letterOneColumn]
+        indexTracker = indexTracker + 2
+    return decryptedText
 
 # Main events start from here
-
 init_matrix(key)
 fiveDMatrix = conver_to_multiverse_list(matrix)
 print_multi_matrix(fiveDMatrix)
-print(matrix)
 
 while exitApplication == False:
     operation = input("Please choose operation (1) Encrypt (2) De-crypt > ")
-    plainInput = input("Enter plain input > ")
-    plainInput = convert_plain_input(plainInput)
-    print(plainInput)
-    encrypt_plain_input(plainInput)
+    if operation == "1":
+        plainInput = input("Enter plain input > ")
+        plainInput = convert_plain_input(plainInput)
+        print("Cipher output: " + encrypt_plain_input(plainInput))
+    elif operation == "2":
+        plainInput = input("Enter cipher input > ")
+        print("Plaintext output: " + decrypt_plain_input(plainInput.upper()))
+    else:
+        print("Error! Please choose on of the options.")
